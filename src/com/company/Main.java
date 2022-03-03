e01b3413c9eb690301ac79e69c42f83c70a75fa2
dantist987 <adik.isak2412@gmail.com>
2022-03-03 15:29:10 +0600

    Dots.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,25 @@ public class Main {
         boss.setDefenceType("Hero.");
 
         System.out.println("Boss health is " + boss.getHealth() + ". Boss damage is " + boss.getDamage() + ". Boss defence type is " + boss.getDefenceType());
+        Hero[] heroes = createHeroes();
+        for (Hero hero : heroes) {
+            String agility;
+            if (hero.getSuperAgility() == null) {
+                agility = ". No Super Agility.";
+            } else {
+                agility = ". Super Agility is " + hero.getSuperAgility();
+            }
+            System.out.println("Hero's " + "Health is " + hero.getHealth() + ". Damage is " + hero.getDamage() + agility);
+        }
     }
+
+    public static Hero[] createHeroes() {
+        Hero Strength = new Hero(400, 60, "Physical.");
+        Hero Magical = new Hero(300, 50);
+        Hero Archer = new Hero(200, 40, "Long Range Shot.");
+
+        return new Hero[]{Strength, Magical, Archer};
+    }
+
+
 }
